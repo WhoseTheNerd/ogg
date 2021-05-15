44ec1b0c0ebd7d10a5ded15187d6527a81505113
WhoseTheNerd <tutorialvideohd@gmail.com>
2021-05-15 15:02:28 +0300

    Add premake 5 file

---
+++ premake5.lua
@@ -0,0 +1,29 @@
+project "ogg"
+    kind "StaticLib"
+    language "C"
+    staticruntime "off"
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "src/**.h",
+        "src/**.c"
+    }
+
+    includedirs
+    {
+        "include"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
